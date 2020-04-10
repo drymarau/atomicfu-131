eac2d7afdbb6d5163204a1f7c5deea9160a0ea0a
Dmitry Rymarev <dmitry.rymarev@gmail.com>
2020-04-10 19:54:20 +0300

    Initial commit

---
+++ event/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform")
+    id("kotlinx-atomicfu")
+}
+
+kotlin {
+    jvm()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
